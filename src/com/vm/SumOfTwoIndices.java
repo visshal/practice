34144881aed3eb706fa1663212896607fd7fe491
Vishal Mehta <vishal.g.mehta@intel.com>
2016-10-24 09:51:52 -0700

    Modified git ignore.

---
+++ src/com/vm/SumOfTwoIndices.java
@@ -0,0 +1,26 @@
+package com.vm;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution.
+ */
+public class SumOfTwoIndices {
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{0, 0};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int[] result = twoSum(nums, 18);
+        for (int i : result)
+            System.out.print(i + " ");
+    }
+}
